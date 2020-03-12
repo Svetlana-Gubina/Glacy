fb1bc375333a626493a3e87ae995577d8ff44150
Svetlana <gubinas83@gmail.com>
2020-03-13 00:53:01 +0300

    Add filter layout & body.js

---
+++ js/popup.js
@@ -0,0 +1,61 @@
+'use strict';
+(function() {
+    var submenu = document.querySelector('#submenu');
+    var cat = document.querySelector('#cat-link');
+    var search = document.querySelector('#search');
+    var searchButton = document.querySelector('#search-button');
+    var loginLink = document.querySelector('#login');
+    var loginForm = document.querySelector('#login-form');
+    var feedbackLink = document.querySelector('.button-feedback');
+    var feedbackForm = document.querySelector('.modal-feedback');
+    var overlay = document.querySelector('.modal-overlay');
+    var btnClose = feedbackForm.querySelector('.modal-close');
+
+    // Открытие и закрытие модального окна
+    var modalClose = function (modal) {
+        modal.classList.add("visually-hidden");
+    };
+    
+    var modalOpen = function (modal) {
+        modal.classList.remove("visually-hidden");
+    };
+
+    cat.addEventListener('mouseenter', function(evt) {
+        modalOpen(submenu);
+        cat.addEventListener('mouseout', function(evt) {
+            modalClose(submenu);
+        });
+    });
+
+    searchButton.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        search.classList.toggle("visually-hidden");
+    });
+
+    loginLink.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        loginForm.classList.toggle("visually-hidden");
+    });
+
+    feedbackLink.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        modalOpen(feedbackForm);
+        modalOpen(overlay);
+    });
+
+    var feedbackClose = function () {
+        modalClose(feedbackForm);
+        modalClose(overlay);
+    };
+
+    btnClose.addEventListener("click", feedbackClose);
+    overlay.addEventListener("click", feedbackClose);
+    // document.addEventListener('keydown', function(evt) {
+    //     evt.preventDefault();
+    //     if (evt.key === 'Escape') {
+    //         modalOpen(feedbackForm);
+    //         modalOpen(overlay);
+    //     }
+    // });
+})();
+
