5c42c2620f39fed8baeafc4c3f01dcbfd71a5daa
Svetlana <gubinas83@gmail.com>
2020-03-13 16:40:40 +0300

    Page-2 - pixel perfect

---
+++ js/popup-index.js
@@ -0,0 +1,38 @@
+'use strict';
+(function() {
+    var feedbackLink = document.querySelector('.button-feedback');
+    var feedbackForm = document.querySelector('.modal-feedback');
+    var overlay = document.querySelector('.modal-overlay');
+    var btnClose = document.querySelector('.modal-close');
+
+     // Открытие и закрытие модального окна
+     var modalClose = function (modal) {
+        modal.classList.add("visually-hidden");
+    };
+    
+    var modalOpen = function (modal) {
+        modal.classList.remove("visually-hidden");
+    };
+
+    feedbackLink.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        modalOpen(feedbackForm);
+        modalOpen(overlay);
+    });
+
+    var feedbackClose = function () {
+        modalClose(feedbackForm);
+        modalClose(overlay);
+    };
+
+    btnClose.addEventListener("click", feedbackClose);
+    overlay.addEventListener("click", feedbackClose);
+    // document.addEventListener('keydown', function(evt) {
+    //     evt.preventDefault();
+    //     if (evt.key === 'Escape') {
+    //         modalOpen(feedbackForm);
+    //         modalOpen(overlay);
+    //     }
+    // });
+})();
+
--- js/popup.js
+++ js/popup.js
@@ -6,19 +6,7 @@
     var searchButton = document.querySelector('#search-button');
     var loginLink = document.querySelector('#login');
     var loginForm = document.querySelector('#login-form');
-    var feedbackLink = document.querySelector('.button-feedback');
-    var feedbackForm = document.querySelector('.modal-feedback');
-    var overlay = document.querySelector('.modal-overlay');
-    var btnClose = feedbackForm.querySelector('.modal-close');
-
-    // Открытие и закрытие модального окна
-    var modalClose = function (modal) {
-        modal.classList.add("visually-hidden");
-    };
     
-    var modalOpen = function (modal) {
-        modal.classList.remove("visually-hidden");
-    };
 
     cat.addEventListener('click', function(evt) {
         evt.preventDefault();
@@ -34,26 +22,5 @@
         evt.preventDefault();
         loginForm.classList.toggle("visually-hidden");
     });
-
-    feedbackLink.addEventListener("click", function(evt) {
-        evt.preventDefault();
-        modalOpen(feedbackForm);
-        modalOpen(overlay);
-    });
-
-    var feedbackClose = function () {
-        modalClose(feedbackForm);
-        modalClose(overlay);
-    };
-
-    btnClose.addEventListener("click", feedbackClose);
-    overlay.addEventListener("click", feedbackClose);
-    // document.addEventListener('keydown', function(evt) {
-    //     evt.preventDefault();
-    //     if (evt.key === 'Escape') {
-    //         modalOpen(feedbackForm);
-    //         modalOpen(overlay);
-    //     }
-    // });
 })();
 
